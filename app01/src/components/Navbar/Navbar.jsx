5c52faec58197a0be31bebd37aa0ef0cbad4b026
Weverton <wevertonbezzera013@gmail.com>
2022-03-10 19:42:24 -0300

    start

---
+++ app01/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Navbar.scss'
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div>
+                <h1>LOGO</h1>
+            </div>
+
+            <lu>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </lu>
+        </nav>
+    )
+}
+
+export default Navbar
