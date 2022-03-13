1f14eb35dddf60a39c5bc1eb703de16a760d7869
Weverton <wevertonbezzera013@gmail.com>
2022-03-13 18:15:07 -0300

    setting

--- app01/src/App.js
+++ app01/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Navbar } from './components'
+import { Header, Footer } from './components/container'
 
 import './App.scss'
 
 const App = () => {
     return (
-        <Navbar/>
+        <div>
+            <Navbar/>
+            <Header/>
+            <Footer/>
+        </div>
     )
 }
 
